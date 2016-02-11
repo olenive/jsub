f0dee4f5800d3200d3ea563a9da30ad57aa065d8
Oleg Lenive <olenive@gmail.com>
2016-02-11 16:54:20 +0000

    Initial commit.  Julia code all in single file called jsub.jl After this commit the intention is to split this file up for ease of use/importing/testing.

---
+++ unit_tests/shell_scripts/write_files/ut_process.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Read arguments
+arg1="$1"
+arg2="$2"
+arg3="$3"
+arg4="$4"
+
+echo "Unit test shell script - Running $0 with arguments: "
+echo "Pretending to process data for each sample. Output file contains the sample ID followed by the reference file.
+
+echo "input sample ID: ""$arg1"
+echo "input file: ""$arg2"
+echo "input list: ""$arg3"
+echo "output file: ""$arg4"
+
+echo "Imaginary prcoessing of sample ""$arg1" > ${arg4}
+echo "Using list of input data from: ${arg3}" >> ${arg4}
+echo "Followd by the reference data: "
+cat ${arg4}  ${arg2} > ${arg4}
+
+echo "finished running $0" using inputs "
+
+
+
