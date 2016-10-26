e36041e19e875a56b6878b043f1e76de49835302
Oleg Lenive <olenive@gmail.com>
2016-10-26 10:56:46 +0100

    WARNING: commit contains broken unit tests.
    Pass it_basic_unzip.sh integration test

--- integration_tests/basic/it_basic_unzip.sh
+++ integration_tests/basic/it_basic_unzip.sh
@@ -7,30 +7,30 @@ set -e
 JOB_HEADER="$1"
 
 PROTOCOL_FILE="../basic.protocol"
-LSF_JOB_NAME="basic_1"
+LSF_JOB_NAME="basic_1_1"
 
 EXPECTED_SUMMARY="../expected_files/basic_1.summary"
 EXPECTED_SUMMARY_LIST="../expected_files/basic.list-summaries"
-EXPECTED_JOB_IN="../expected_files/basic_1.lsf"
-EXPECTED_JOB_LIST="../expected/basic.list-jobs"
+EXPECTED_JOB_IN="../expected_files/${LSF_JOB_NAME}.lsf"
+EXPECTED_JOB_LIST="../expected_files/basic.list-jobs"
 EXPECTED_JOB_DATA="../expected_files/it1_basic.txt"
 EXPECTED_COMPLETED="../expected_files/basic_1.completed"
-EXPECTED_PROTABLE_DIR="../expected_files/portable"
-EXPECTED_PORTABLE_ZIP="$EXPECTED_PROTABLE_DIR".tar.gz
+EXPECTED_PORTABLE_DIR="../expected_files/portable"
+EXPECTED_PORTABLE_ZIP="$EXPECTED_PORTABLE_DIR".tar.gz
 
 GENERATED_DIR="generated_files_zip"
 GENERATED_SUMMARY="basic_1.summary"
 GENERATED_SUMMARY_LIST="basic.list-summaries"
-GENERATED_JOB_IN="basic_1.lsf"
+GENERATED_JOB_IN="${LSF_JOB_NAME}.lsf"
 GENERATED_JOB_LIST="basic.list-jobs"
 GENERATED_JOB_DATA="it1_basic.txt"
-GENERATED_JOB_OUTPUT="basic_1.error"
-GENERATED_JOB_ERROR="basic_1.output"
+GENERATED_JOB_OUTPUT="${LSF_JOB_NAME}.error"
+GENERATED_JOB_ERROR="${LSF_JOB_NAME}.output"
 GENERATED_SUBMITTED_JOBS_LIST="basic.list-jobs.submitted"
-GENERATED_COMPLETED="basic_1.completed"
-GENERATED_INCOMPLETE="basic_1.incomplete"
-GENERATED_PROTABLE_DIR="portable"
-GENERATED_PORTABLE_ZIP="$GENERATED_PROTABLE_DIR".tar.gz
+GENERATED_COMPLETED="${LSF_JOB_NAME}.completed"
+GENERATED_INCOMPLETE="${LSF_JOB_NAME}.incomplete"
+GENERATED_PORTABLE_DIR="portable"
+GENERATED_PORTABLE_ZIP="$GENERATED_PORTABLE_DIR".tar.gz
 
 CALL_JSUB="julia ../../../jsub.jl -d -v "
 
@@ -42,11 +42,7 @@ echo ""
 echo "Running integration test: basic_zip..."
 
 ### FUNCTIONS ###
-source ../common_it_functions.sh
-# function file_exists {
-#   if [ -f "$1" ]; then echo "yes"; else echo "no"; fi
-# }
-## Overwrite the fnction in common_it_functions to deal with the directory produced by unzipping
+source "../common_it_functions.sh"
 function clear_generated {
   rm -f ${GENERATED_SUMMARY}
   rm -f ${GENERATED_SUMMARY_LIST}
@@ -58,46 +54,8 @@ function clear_generated {
   rm -f ${GENERATED_SUBMITTED_JOBS_LIST}
   rm -f ${GENERATED_COMPLETED}
   rm -f ${GENERATED_INCOMPLETE}
-  rm -fr ${GENERATED_PROTABLE_DIR}
-}
-# function isAbsolutePath {
-#   local DIR="$1"
-#   [[ ${DIR:0:1} == '/' ]] && echo "absolute" || echo "relative"
-# }
-# function isJobNameInQueue {
-#   local jobName="$1"
-#   local res=$(bjobs -J ${jobName})
-#   if [ "$res" = "" ]; then
-#     echo "no"
-#   else
-#     echo "yes"
-#   fi
-# }
-# function awaitJobNameCompletion {
-#   echo "Waiting for completion of job named ""$1"
-#   while [ $(isJobNameInQueue "$1") == "yes" ]; do
-#     sleep 1
-#   done
-#   echo "...job presumed to be completed."
-# }
-# # Function used to determine the require option (-c) and file path for the header file containing text included in all jobs
-# function getCommonHeaderOptionString {
-#   if [ "$1" == "" ]; then
-#     echo ""
-#   elif [ $(isAbsolutePath "$1") == "relative" ]; then
-#     echo " -c ../""$1"
-#   else
-#     echo " -c ""$1"
-#   fi
-# }
-function forcePathAbsolute {
-  if [ "$1" == "" ]; then
-    echo ""
-  elif [ $(isAbsolutePath "$1") == "relative" ]; then
-    echo "$PWD"/"$1"
-  else
-    echo "$1"
-  fi
+  rm -fr ${GENERATED_PORTABLE_DIR}
+  rm -fr ${GENERATED_PORTABLE_ZIP}
 }
 #################
 
@@ -114,36 +72,44 @@ cd ${GENERATED_DIR}
 # ${CALL_JSUB} -s -p ${PROTOCOL_FILE}
 # # Check that a summary file and a summary listing file are generated from the protocol
 # assert "file_exists ${GENERATED_SUMMARY}" "yes"
-# assert "diff ${GENERATED_SUMMARY} ${EXPECTED_SUMMARY}" ""
+# assert "compare_contents ${GENERATED_SUMMARY} ${EXPECTED_SUMMARY}" ""
 # assert "file_exists ${GENERATED_SUMMARY_LIST}" "yes"
-# assert "diff ${GENERATED_SUMMARY_LIST} ${EXPECTED_SUMMARY_LIST}" ""
-
+# assert "compare_contents ${GENERATED_SUMMARY_LIST} ${EXPECTED_SUMMARY_LIST}" ""
+# echo ""
+# echo "##################################################"
+# echo ""
 # # Run jsub - create job file from previously generated summary
 # # OPTION_HEADER=$(getCommonHeaderOptionString "$JOB_HEADER")
 # echo "${CALL_JSUB} -j -u ${GENERATED_SUMMARY_LIST} $(getCommonHeaderOptionString "$JOB_HEADER")"
 # ${CALL_JSUB} -j -u ${GENERATED_SUMMARY_LIST} $(getCommonHeaderOptionString "$JOB_HEADER")
 # # Check that a job file is generated from the summary file
 # assert "file_exists ${GENERATED_JOB_IN}" "yes"
-# assert "diff -I '^# --- From file:*' -I "'^#BSUB ?P*'" ${GENERATED_JOB_IN} ${EXPECTED_JOB_IN}" "" # Ignore the line that contain absolute paths or the job header prefix
+# assert "compare_contents ${GENERATED_JOB_IN} ${EXPECTED_JOB_IN} -I '^# --- From file:*' -I '^#BSUB -P*' -I '^#BSUB -q*' -I '^JSUB_PATH_TO_THIS_JOB=*' " "" # Ignore the line that contain absolute paths or the job header prefix -P option.
 # assert "file_exists ${GENERATED_JOB_LIST}" "yes"
-# assert "diff ${GENERATED_JOB_LIST} ${EXPECTED_JOB_LIST}" ""
-
+# assert "compare_contents ${GENERATED_JOB_LIST} ${EXPECTED_JOB_LIST}" ""
+# echo ""
+# echo "##################################################"
+# echo ""
 # # Run jsub - stage 3 but instead of submitting copy jobs to a directory and zip it
 # rm -fr ${GENERATED_PORTABLE_ZIP}
-# ${CALL_JSUB} -b -z -a ${GENERATED_PROTABLE_DIR} -o ${GENERATED_JOB_LIST}
+# ${CALL_JSUB} -b -z -a ${GENERATED_PORTABLE_DIR} -o ${GENERATED_JOB_LIST}
 # awaitJobNameCompletion "$LSF_JOB_NAME"
-# assert "file_exists ${GENERATED_PROTABLE_DIR}/${GENERATED_JOB_IN}" "yes"
-# assert "file_exists ${GENERATED_PROTABLE_DIR}/${GENERATED_JOB_LIST}" "yes"
-# assert "file_exists ${GENERATED_PROTABLE_DIR}/submit_lsf_jobs.sh" "yes"
-# assert "file_exists ${GENERATED_PROTABLE_DIR}/job_submission_functions.sh" "yes"
+# assert "file_exists ${GENERATED_PORTABLE_DIR}/${GENERATED_JOB_IN}" "yes"
+# assert "file_exists ${GENERATED_PORTABLE_DIR}/${GENERATED_JOB_LIST}" "yes"
+# assert "file_exists ${GENERATED_PORTABLE_DIR}/submit_lsf_jobs.sh" "yes"
+# assert "file_exists ${GENERATED_PORTABLE_DIR}/job_submission_functions.sh" "yes"
 # assert "file_exists ${GENERATED_PORTABLE_ZIP}" "yes"
+# echo ""
+# echo "##################################################"
+# echo ""
 
-clear_generated # Remove existing output from previous tests
+# clear_generated # Remove existing output from previous tests
+
 # Extract zipped jobs and submit them
 tar -zxvf ${GENERATED_PORTABLE_ZIP}
-rm -rf ../${GENERATED_PROTABLE_DIR}
-mv ${GENERATED_PROTABLE_DIR} .. # Too lazy to change the relative path in the protocol for this test alone
-cd ../${GENERATED_PROTABLE_DIR}
+rm -rf ../${GENERATED_PORTABLE_DIR}
+mv ${GENERATED_PORTABLE_DIR} .. # So as not to change the relative path in the protocol for this test alone
+cd ../${GENERATED_PORTABLE_DIR}
 # Manually combine the generated job and the rquired local system header information
 if [[ ${ABSOLUTE_HEADER_PATH} != "" ]] && [[ ${GENERATED_JOB_IN} != "" ]]; then
   cat ${ABSOLUTE_HEADER_PATH} >> ${GENERATED_JOB_IN}
