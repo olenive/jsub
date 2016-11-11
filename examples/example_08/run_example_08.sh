e7a7b238166e72bcabed30345f8996ee69378a0b
Oleg Lenive <olenive@gmail.com>
2016-11-11 11:29:34 +0000

    WARNING: commit contains failing integration tests
    Create example 8

--- examples/example_08/run_example_08.sh
+++ examples/example_08/run_example_08.sh
@@ -4,7 +4,8 @@ jsub="julia ../../jsub.jl "
 ## Run example code
 mkdir -p dummy_output # create the directory for job results as indicated in the vars07.vars file
 
-$jsub --protocol cat07.protocol \
+$jsub --generate-summaries --generate-jobs \
+     --protocol cat08.protocol \
      --vars vars07.vars \
      --fvars fvars07.fvars \
      --header-from-file "../my_job_header.txt" \
@@ -13,3 +14,5 @@ $jsub --protocol cat07.protocol \
      --prefix-lsf-out "lsf_out/" \
      --prefix-completed "progoress/completed/" \
      --prefix-incomplete "progoress/incomplete/"
+
+$jsub --submit-jobs --list-jobs "cat08_vars07_fvars07.list-jobs"
