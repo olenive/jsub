4f73e343bf9ccdf02992bbf8d75f86f13459639d
Oleg Lenive <olenive@gmail.com>
2016-11-14 10:15:56 +0000

    WARNING: commit contains failing integration tests
    Add all integration tests to run_all_integration_tests.sh
    Make run_all_integration_tests.sh ask for a path to the LSF job header
    file

--- integration_tests/run_all_integration_tests.sh
+++ integration_tests/run_all_integration_tests.sh
@@ -2,9 +2,27 @@
 
 # Script for running all integration tests.  The first argument is a path to a file containing LSF options specific to the system that the test is being run on
 DATETIME=`date +%Y%m%d_%H%M%S`
-FILE_LSF_COMMON=$(readlink -e "$1")
+INPUT_FILE_LSF_COMMON="$1"
 TEST_OUTPUT_LOG=$(pwd)/integration_test_run.log
 DIR_ORIGIN=$(pwd)
+
+# Clear log file
+echo "Writing output and errors to log file: ""$TEST_OUTPUT_LOG"
+echo "Runnign integration tests on ""$DATETIME" > "$TEST_OUTPUT_LOG"
+
+## Prompt the user to provide a path to their job header file (specific to their LSF account and/or system)
+if [ "$INPUT_FILE_LSF_COMMON" = "" ]; then
+  echo "Please provide a path to your LSF header file: "
+  read INPUT_FILE_LSF_COMMON
+fi
+FILE_LSF_COMMON=$(pwd)/"$INPUT_FILE_LSF_COMMON"
+
+
+echo "Running integration tests using the following job header from file: ""$FILE_LSF_COMMON"
+echo "Running integration tests using the following job header from file: ""$FILE_LSF_COMMON" >>"$TEST_OUTPUT_LOG" 2>&1
+cat "$FILE_LSF_COMMON"
+cat "$FILE_LSF_COMMON" >> "$TEST_OUTPUT_LOG" 2>&1
+echo ""
 
 # Check if the last line of the log corresponds to all tests passing
 function check_success {
@@ -28,17 +46,15 @@ function run_test {
   cd "$DIR_ORIGIN"
 }
 
-# Clear log file
-echo "Running all integration tests and writing output and errors to log file: ""$TEST_OUTPUT_LOG"
-echo "Using local LSF options from: ""$FILE_LSF_COMMON"
-echo "Runnign integration tests on ""$DATETIME" > "$TEST_OUTPUT_LOG"
-
+## Run tests
 run_test basic/it_basic.sh
+
 run_test basic/it_basic_zip.sh
-sleep 1
+
 run_test basic/it_basic_unzip.sh
 
 run_test fvars/it_fvars.sh
+
 run_test fvars/it_fvars_prefixes.sh
 
 run_test vars_fvars/it_fvars_vars.sh
@@ -46,5 +62,17 @@ run_test vars_fvars/it_fvars_vars.sh
 run_test jgroup/it_jgroup.sh
 
 run_test jgroup_checkpoints/it_jgroup_checkpoints.sh
+
+run_test jgroup_checkpoints_incomplete/it_jgroup_checkpoints_incomplete.sh
+
+run_test jgroup_prefixCompleted/it_jgroup_prefixCompleted.sh
+
+run_test example_04_it/it_example_04.sh
+
+run_test jgroup_prefixCompleted/it_jgroup_prefixCompleted.sh
+
+run_test example_04_it/it_example_04.sh
+
+run_test examples_it/it_examples.sh
 
 # EOF
